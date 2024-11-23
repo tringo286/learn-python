d4610e90aa715bd53c3d07da5a87095d7566440e
Tri Ngo <tringo831@gmail.com>
2024-11-22 21:33:12 -0800

    Add lesson 4: madlibs game

---
+++ lessions/lesson-4-madlibs-game/madlibs_game.py
@@ -0,0 +1,14 @@
+# Madlibs games
+# word game where you create a story 
+# by filling in blanks with random words
+
+adjective1 = input('Enter an adjective: ')
+noun1 = input('Enter an noun: ')
+adjective2 = input('Enter an adjective: ')
+verb1 = input('Enter an verb ending with ing: ')
+adjective3 = input('Enter an adjective: ')
+
+print(f'Today I went to a {adjective1} zoo.')
+print(f'In a exhibit, I saw a {noun1}')
+print(f'{noun1} was {adjective2} and {verb1}')
+print(f'I was {adjective3}!')
