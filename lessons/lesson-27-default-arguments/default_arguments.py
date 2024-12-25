34fe409d717e5db488028b5439cfb5e92148335f
Tri Ngo <tringo831@gmail.com>
2024-12-24 22:31:58 -0800

    Add lesson 27: default arguments

---
+++ lessons/lesson-27-default-arguments/default_arguments.py
@@ -0,0 +1,24 @@
+# default arguments = A default value for certain parameters 
+#                     default is used when that arguments is omitted
+#                     make your functions more flexiable, reduces # of arguments
+#                     1. positional, 2. DEFAULT, 3. keyword, 4. arbitrary
+
+import time
+
+# Example #1
+def net_price(list_price, discount=0, tax=0.05):
+    return list_price * (1 - discount) * (1 + tax)
+
+print(net_price(500)) # 525
+print(net_price(500, 0.1)) # 472.5
+print(net_price(500, 0.1, 0)) # 450
+
+# Example #2 
+def count(end, start=0):
+    for x in range(start, end+1):
+        print(x)
+        time.sleep(1)
+    print('DONE!')
+
+count(5) # 0 1 2 3 4 5 DONE!
+count(5, 2) # 2 3 4 5 DONE!
